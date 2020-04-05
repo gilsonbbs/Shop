fc78c6cff0b80cdae9d35970cc078ccddffc3991
NosotrosSSD <NosotrosSSD@DESKTOP-BBRQLNO>
2020-04-05 18:37:11 +0200

    segundo_comit_rama_primera_fase

---
+++ Shop/Web.Shop/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Shop.Data.Entities;
+
+namespace Web.Shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
