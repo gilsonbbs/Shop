7ea027ea043e41a00a6d30985c32b96a22a045b7
NosotrosSSD <NosotrosSSD@DESKTOP-BBRQLNO>
2020-04-09 14:37:16 +0200

    09_04

--- Shop/Web.Shop/Startup.cs
+++ Shop/Web.Shop/Startup.cs
@@ -33,6 +33,8 @@ namespace Web.Shop
 
             services.AddTransient<SeedDb>();
 
+            services.AddScoped<IRepository, Repository>();
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
