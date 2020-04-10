ef9a27d2fafeb2a845d7a75ab19f6009ec7153d7
NosotrosSSD <NosotrosSSD@DESKTOP-BBRQLNO>
2020-04-10 17:31:34 +0200

    starup arreglado

--- Shop/Web.Shop/Startup.cs
+++ Shop/Web.Shop/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Web.Shop.Data;
+using Web.Shop.Data.Entities;
 
 namespace Web.Shop
 {
@@ -26,6 +28,19 @@ namespace Web.Shop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<User, IdentityRole>
+                (
+                
+                cfg => { cfg.User.RequireUniqueEmail = true; 
+                cfg.Password.RequireDigit = false; 
+                cfg.Password.RequiredUniqueChars = 0; 
+                cfg.Password.RequireLowercase = false; 
+                cfg.Password.RequireNonAlphanumeric = false; 
+                cfg.Password.RequireUppercase = false; }
+                
+                ).AddEntityFrameworkStores<DataContext>();
+
+
             services.AddDbContext<DataContext>(cfg =>
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
@@ -61,6 +76,7 @@ namespace Web.Shop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
