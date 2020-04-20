3707949518024c7c3a91ab595d9c5a1ccb29ae68
NosotrosSSD <NosotrosSSD@DESKTOP-BBRQLNO>
2020-04-20 11:38:52 +0200

    api nueva

---
+++ Shop/Web.Shop/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿namespace Web.Shop.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Web.Shop.Data;
+
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
