bf793e9440fff2fd77fb8ced90cabb1f9cbcb4f5
NosotrosSSD <NosotrosSSD@DESKTOP-BBRQLNO>
2020-05-04 14:38:35 +0200

    xamarin_first_page_login

---
+++ Shop/Shop.UIForms/Shop.UIForms/Infrastructure/Intancelocator.cs
@@ -0,0 +1,17 @@
+﻿namespace Shop.UIForms.Infrastructure
+{
+    using Shop.UIForms.ViewModels;
+
+    public class Intancelocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public Intancelocator()
+
+        {
+            this.Main = new MainViewModel();
+        }
+
+
+    }
+}
